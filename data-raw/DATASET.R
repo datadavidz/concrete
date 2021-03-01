4007f398ec58b8a5bf46c41228849eaa4d4c082e
datadavidz <david.l.zoller@gmail.com>
2021-03-01 10:32:47 -0600

    Add specific volume data to package as spec_vol

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,6 @@
 ## code to prepare `DATASET` dataset goes here
 
 usethis::use_data(model, overwrite = TRUE)
+
+spec_vol <- readRDS("./data-raw/concrete_spec_vol.rds")
+usethis::use_data(spec_vol, overwrite = TRUE)
