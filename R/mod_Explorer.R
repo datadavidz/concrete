b51513189d745ea80fd02c9c382c2fec2164364d
datadavidz <david.l.zoller@gmail.com>
2021-03-01 09:34:13 -0600

    Pass formula to Explorer module

--- R/mod_Explorer.R
+++ R/mod_Explorer.R
@@ -10,16 +10,29 @@
 mod_Explorer_ui <- function(id){
   ns <- NS(id)
   tagList(
-    h1("concrete")
+    h1("concrete"),
+    verbatimTextOutput(ns("formula"))
   )
 }
     
 #' Explorer Server Function
 #'
 #' @noRd 
-mod_Explorer_server <- function(input, output, session){
+mod_Explorer_server <- function(input, output, session, formulation){
   ns <- session$ns
- 
+  
+  output$formula <- renderText({
+    #paste(c("Cement:", formulation()$cement))
+    glue::glue("Cement: {formulation()$cement} kg
+                Blast Furnace Slag: {formulation()$blast_furnace_slag} kg
+                Fly Ash: {formulation()$fly_ash} kg
+                Water: {formulation()$water} kg
+                Superplasticizer: {formulation()$superplasticizer} kg
+                Coarse Aggregate: {formulation()$coarse_aggregate} kg
+                Fine Aggregate: {formulation()$fine_aggregate} kg
+                Age: {formulation()$age} days")
+  })
+  
 }
     
 ## To be copied in the UI
